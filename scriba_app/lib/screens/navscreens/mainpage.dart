3708095fce200a5b377615f6ad1bdc5d80b40cd0
SamihaHussein2 <samiha1800973@miuegypt.edu.eg>
2022-03-05 21:22:03 +0200

    Upload-Update

--- scriba_app/lib/screens/navscreens/mainpage.dart
+++ scriba_app/lib/screens/navscreens/mainpage.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:scriba_app/defaults/theme.dart';
 import 'package:scriba_app/screens/navscreens/homepage.dart';
 import '/widgets/button_widget.dart';
@@ -13,6 +16,8 @@ class MainPage extends StatefulWidget {
 }
 
 class _HomepageState extends State<MainPage> {
+  var ipUrl = "http://192.168.95.1:8003/image";
+
   File? img;
   Future ImportImage() async {
     try {
@@ -26,62 +31,107 @@ class _HomepageState extends State<MainPage> {
     }
   }
 
+  Future CaptureImage() async {
+    try {
+      final img = await ImagePicker().pickImage(source: ImageSource.camera);
+      if (img == null) return;
+
+      final TakenImage = File(img.path);
+      setState(() => this.img = TakenImage);
+    } on PlatformException catch (e) {
+      print("Failed to pick image: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-          body: Container(
+      body: Container(
         constraints: const BoxConstraints.expand(),
         decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("assets/images/Back-dark.png"),
                 fit: BoxFit.cover)),
-        child: ListView(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    primary: AppTheme.darkRed,
-                  ),
-                  child: Container(
-                      width: 100,
-                      height: 100,
-                      alignment: Alignment.center,
-                      decoration: const BoxDecoration(shape: BoxShape.circle),
-                      child: Image.asset(
-                        "assets/images/homepage_icons/Import.png",
-                        width: 70,
-                      )),
-                  onPressed: () {
-                    ImportImage();
-                  },
+        child: ListView(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  primary: AppTheme.darkRed,
                 ),
-                SizedBox(
-                  width: 40,
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: const CircleBorder(),
-                    primary: AppTheme.darkRed,
-                  ),
-                  child: Container(
+                child: Container(
                     width: 100,
                     height: 100,
                     alignment: Alignment.center,
                     decoration: const BoxDecoration(shape: BoxShape.circle),
-                    child: Icon(
-                      Icons.camera,
-                      color: AppTheme.moderateOrange,
-                      size: 50,
-                    ),
+                    child: Image.asset(
+                      "assets/images/homepage_icons/Import.png",
+                      width: 70,
+                    )),
+                onPressed: () {
+                  ImportImage();
+                },
+              ),
+              SizedBox(
+                width: 40,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: const CircleBorder(),
+                  primary: AppTheme.darkRed,
+                ),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(shape: BoxShape.circle),
+                  child: Icon(
+                    Icons.camera,
+                    color: AppTheme.moderateOrange,
+                    size: 50,
                   ),
-                  onPressed: () {},
+                ),
+                onPressed: () {
+                  CaptureImage();
+                },
+              )
+            ],
+          ),
+          img == null
+              ? Text(
+                  'Please upload or capture an image',
                 )
-              ],
-            ),
-          ],
-        ),
+              : Image.file(img!),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          try {
+            final bytes = File(img!.path).readAsBytesSync();
+            String img64 = base64Encode(bytes);
+            final Dio _dio = Dio();
+            _dio.post(
+              ipUrl,
+              data: {"image": img64},
+            ).then((value) {
+              print(value.data['status']);
+              if (value.data['status'] == "Image Opened") {
+                Fluttertoast.showToast(
+                  msg: "Image Sent To Backend",
+                  toastLength: Toast.LENGTH_SHORT,
+                  gravity: ToastGravity.BOTTOM,
+                  backgroundColor: Colors.green,
+                  textColor: Colors.white,
+                  fontSize: 16.0,
+                );
+              }
+            });
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.send),
       ));
 }
