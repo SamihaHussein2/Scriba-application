9bd50085e439351060b604ca13b23f6e87ab8572
FaridaHesham99 <83432002+FaridaHesham99@users.noreply.github.com>
2022-05-26 02:25:10 +0200

    Updated main

--- scriba_app/lib/main.dart
+++ scriba_app/lib/main.dart
@@ -20,7 +20,7 @@ Future<void> main() async {
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(
-        create: (_) => MythNotifier(),
+        create: (context) => MythNotifier(),
       ),
     ],
     child: MyApp(),
@@ -29,20 +29,8 @@ Future<void> main() async {
   //runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    Future<void> mythNotifier =
-        Provider.of<MythNotifier>(context, listen: false).fetchMyths();
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,15 +38,6 @@ class _MyAppState extends State<MyApp> {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: Splash(),
